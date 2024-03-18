f001f7ff0c6e6497fc3f39076dc73fc7e7295a1a
Luis Campos <luis.campos5489@alumnos.udg.mx>
2024-03-17 22:12:18 -0600

    uso de sesiones

--- routes/web.php
+++ routes/web.php
@@ -14,27 +14,38 @@ use App\Http\Controllers\EmpleadosController;
 |
 */
 
-Route::get('/empleados/index', [EmpleadosController::class, 'index'])->name('empleados.index');
-Route::get('/empleados/crear', [EmpleadosController::class, 'create'])->name('empleados.create');
-Route::post('/empleados', [EmpleadosController::class, 'store'])->name('empleados.store');
-
-Route::get('/empleados/edit/{empleados}', [EmpleadosController::class, 'edit'])->name('empleados.edit');
-Route::post('/empleados/{empleados}', [EmpleadosController::class, 'update'])->name('empleados.update');
-
-Route::get('/empleados/{empleado}', [EmpleadosController::class, 'show'])->name('empleados.show');
-
-Route::delete('/empleados/{empleado}', [EmpleadosController::class, 'destroy'])->name('empleados.destroy');
-
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/empleados', function () {
-    return view('inicio_empleados');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    
+    
+    Route::get('/empleados', function () {
+        return view('inicio_empleados');
+    });
+
+    Route::get('/empleados/crear', function () {
+        return view('crear_empleado');
+    });
+    
+    Route::get('/empleados/index', [EmpleadosController::class, 'index'])->name('empleados.index');
+    Route::get('/empleados/crear', [EmpleadosController::class, 'create'])->name('empleados.create');
+    Route::post('/empleados', [EmpleadosController::class, 'store'])->name('empleados.store');
+    
+    Route::get('/empleados/edit/{empleados}', [EmpleadosController::class, 'edit'])->name('empleados.edit');
+    Route::post('/empleados/{empleados}', [EmpleadosController::class, 'update'])->name('empleados.update');
+    
+    Route::get('/empleados/{empleado}', [EmpleadosController::class, 'show'])->name('empleados.show');
+    
+    Route::delete('/empleados/{empleado}', [EmpleadosController::class, 'destroy'])->name('empleados.destroy');
 });
 
-Route::get('/empleados/crear', function () {
-    return view('crear_empleado');
-});
-
+?>
